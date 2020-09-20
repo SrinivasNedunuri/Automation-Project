f5fb3f716cf8c2fd1d224ef109512c7c20b58a56
Srinivas <54639431+SrinivasNedunuri@users.noreply.github.com>
2020-09-19 20:12:50 -0400

    generating report to let know the supplier about the imported data

--- new_reports/reports.py
+++ new_reports/reports.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+#generating report to let know the supplier about the imported data
 from reportlab.platypus import SimpleDocTemplate
 from reportlab.platypus import Paragraph, Spacer, Table, Image
 from reportlab.lib.styles import getSampleStyleSheet
